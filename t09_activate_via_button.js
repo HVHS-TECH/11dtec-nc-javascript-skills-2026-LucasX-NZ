e3c3ace7983c8d9c54251d0ba27696b570d7e40e
LucasX-NZ <24160ls@hvhs.school.nz>
2026-05-07 21:17:46 +0000

    May 7, 2026, 9:17 PM

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -1,1 +1,26 @@
-/* Paste your code from task 7 here*/
+/****************************
+Name of task:
+****************************/
+console.log("Running t06_functions.js")
+
+//Variables
+
+/****************************
+Main Code
+****************************/
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+OUTPUT.innerHTML = "<h1>Hello World!</h1>";
+
+/****************************
+Functions
+****************************/
+
+function displayWelcome(_name, _products) {
+    OUTPUT.innerHTML += "<h2>The products for " + _name + " are: " + _products + "</h2>";
+};
+
+function start() {
+    displayWelcome("Alice", "Chocolate bar: $4, Chips: $3, Drink: $2.50");
+}
+
