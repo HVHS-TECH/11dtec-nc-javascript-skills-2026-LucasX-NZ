0082cb5ab4186c0a5096f97b38615b1bd3e91a8d
LucasX-NZ <24160ls@hvhs.school.nz>
2026-05-07 21:46:55 +0000

    May 7, 2026, 9:46 PM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -1,1 +1,26 @@
-/* Paste your code from the last task */
+/****************************
+Name of task:
+****************************/
+console.log("Running t06_functions.js")
+
+//Variables
+
+/****************************
+Main Code
+****************************/
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+OUTPUT.innerHTML = "<h1>Hello World!</h1>";
+
+/****************************
+Functions
+****************************/
+
+function displayWelcome(_name, _products) {
+    OUTPUT.innerHTML += "<h2>The products for " + _name + " are: " + _products + "</h2>";
+};
+
+function start() {
+    displayWelcome("Alice", "Chocolate bar: $4, Chips: $3, Drink: $2.50");
+}
+
