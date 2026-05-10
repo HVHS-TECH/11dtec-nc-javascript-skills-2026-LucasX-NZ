bdeed1ef71152d43562c14f8653c3a3c4c0fc7d0
LucasX-NZ <24160ls@hvhs.school.nz>
2026-05-10 22:51:37 +0000

    May 10, 2026, 10:51 PM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -13,8 +13,8 @@ const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 OUTPUT.innerHTML = "<h1>Hello World!</h1>";
 
 <form>
-    <input type="text">
-    <input type="submit">
+    <input type="text" placeholder="Put name here"></input>
+    <input type="submit"></input>
 </form>
 
 /****************************
