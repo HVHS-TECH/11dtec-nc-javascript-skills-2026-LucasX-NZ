93753e6427c05e6c9b922b95ff3a13d0de36ef9a
LucasX-NZ <24160ls@hvhs.school.nz>
2026-05-11 23:59:48 +0000

    May 11, 2026, 11:59 PM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -12,21 +12,16 @@ Main Code
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 OUTPUT.innerHTML = "<h1>Hello World!</h1>";
 
-<form>
-    <label for="formname">Name:</label>
-    <input type="text" name="formname">
-    <input type="submit">
-</form>
+const FORMNAME = document.getElementById("formedname");
+let name = FORMNAME.value;
 
 /****************************
 Functions
 ****************************/
 
-function displayWelcome(_name, _products) {
-    OUTPUT.innerHTML += "<h2>The products for " + _name + " are: " + _products + "</h2>";
-};
-
-function start() {
-    displayWelcome("Alice", "Chocolate bar: $4, Chips: $3, Drink: $2.50");
+function getName() {
+    const FORMNAME = document.getElementById("formedname");
+    let username = FORMNAME.value;
+    OUTPUT.innerHTML = "<h2>Hello " + username + "!</h2>";
 }
 
